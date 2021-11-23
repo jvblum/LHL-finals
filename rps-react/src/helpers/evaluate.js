5e67714f0bd6a6644266a6027e6c5c0e661dcf62
jvblum <jvmell.skch@gmail.com>
2021-11-23 11:07:02 -0500

    implement socket, basic functionality

--- rps-react/src/helpers/evaluate.js
+++ rps-react/src/helpers/evaluate.js
@@ -10,20 +10,16 @@ export const evaluate = (pickA, pickB) => {
     [might]: [rock, paper, scissors]
   }; // what beats what
 
+  let message = "Tie Game!";
   const point = pickA.rating * pickB.rating;
+
   if (rules[pickA.type].includes(pickB.type)) {
-    return `Player A Wins! + ${point}`;
+    message = `Player A Wins! + ${point}`;
   }
 
   if (rules[pickB.type].includes(pickA.type)) {
-    return `Player B Wins! + ${point}`;
+    message = `Player B Wins! + ${point}`;
   } // specific to case where pickB === defend
 
-  if (pickA.type === pickB.type) {
-    return "Tie Game!";
-  }
-
-  if (pickA.type === defend || pickB.type === defend) {
-    return "Tie Game!";
-  } // comes after so "defend beats "might" before it forces a tie
+  return { pickA, pickB, message, point };
 }; // accepts pick objects and returns with results; will incorporate multiplier
