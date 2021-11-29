2dd1dee2c2cd67b68262ace7702b03dbad22df0e
jvblum <jvmell.skch@gmail.com>
2021-11-29 12:13:16 -0500

    style deck component

--- rps-react/src/components/Deck.jsx
+++ rps-react/src/components/Deck.jsx
@@ -2,7 +2,7 @@ export default function Card(props) {
   const deck = props.yourDeck;
   const top = deck[3];
   const deckInfo = [...deck].slice(3).map((card, index) => {
-    return <span key={index}><img src={card.img}/><div>{index}</div></span>;
+    return <span key={index}><img src={card.img}/><div>{index + 1}</div></span>;
   })
   // there is no hand state
   // the first three card in the deck are hand cards
@@ -10,15 +10,17 @@ export default function Card(props) {
   // props.you conditional to show deck content
 
   return (
+    // button tag to align
     <button
       className="Deck"
-      style={{
-        background: `url(${top ? top.img : null})`
-      }}
       disabled
     >
-      {deck.length}
-      {/* {top ? top.type + top.rating : "0"} */}
+      {top !== undefined &&
+      <>
+      <br/>
+      <img src={top.img} alt={`${top.type} + ${top.rating}`}/>
+      <div>{deckInfo.length}</div>
+      </>}
       {props.you && <div className="deck-info">{deckInfo}</div>}
     </button>
   );
