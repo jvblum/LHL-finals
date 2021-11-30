70bbbb265aa951cd062e16aa2efbfaad6210d2c4
jvblum <jvmell.skch@gmail.com>
2021-11-30 17:56:58 -0500

    add app title

--- rps-react/src/App.js
+++ rps-react/src/App.js
@@ -1,8 +1,12 @@
 import "./styles.css";
 
 import Landing from "./components/Landing";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "RPS+DM";
+  }, []);
   return (
     <Landing />
   );
